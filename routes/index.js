f54b8ea1a3039902a70c1347a88fb4149db8b961
Linda Njau <lindanjau21@gmail.com>
2024-03-12 13:14:25 +0300

    Registering routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 import AppController from '../controllers/AppController';
+import UserController from '../controllers/UserController';
+import AuthController from '../controllers/AuthController';
 
 const express = require('express');
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UserController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UserController.getMe);
 
 export default router;
