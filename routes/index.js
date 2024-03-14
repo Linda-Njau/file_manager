207784e35237c3d75e8548734134cbdcc661f7c5
Linda Njau <lindanjau21@gmail.com>
2024-03-14 14:49:25 +0300

    Adding file functionality

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import AppController from '../controllers/AppController';
 import UserController from '../controllers/UserController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const express = require('express');
 const router = express.Router();
@@ -11,5 +12,6 @@ router.post('/users', UserController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UserController.getMe);
+router.post('/files', FilesController.postUpload);
 
 export default router;
